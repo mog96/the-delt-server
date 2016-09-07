f8f468bf8310d02686632e502924d8299e6255c9
Mateo Garcia <mateog@stanford.edu>
2016-09-06 23:40:29 -0300

    first commit baybay

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // compatible API routes.
 
 var express = require('express');
+var S3Adapter = require('parse-server').S3Adapter;
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
@@ -14,12 +15,18 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  fileKey: process.env.FILE_KEY,
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  filesAdapter: new S3Adapter(
+    process.env.AWS_ACCESS_KEY_ID,
+    process.env.AWS_SECRET_ACCESS_KEY,
+    process.env.BUCKET_NAME
+  )
+  // liveQuery: {
+  //   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  // }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
