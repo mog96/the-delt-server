bdef1eabf245a16a9eb0872257ec25bc1c625fb9
Mateo Garcia <mateog@stanford.edu>
2016-12-10 19:06:58 -0800

    Added push configutation to index.js

--- index.js
+++ index.js
@@ -13,22 +13,31 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri,
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY,
   fileKey: process.env.FILE_KEY,
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL,
   filesAdapter: new S3Adapter(
     process.env.AWS_ACCESS_KEY_ID,
     process.env.AWS_SECRET_ACCESS_KEY,
     process.env.BUCKET_NAME,
     {directAccess: true}
-  )
+  ),
+  push: {
+    ios: {
+      pfx: process.env.APS_PRODUCTION_CERT_PATH, // the path and filename to the .p12 file you exported earlier.
+      passphrase: process.env.APS_PRODUCTION_CERT_PASSPHRASE,
+      bundleId: process.env.APS_APP_BUNDLE_ID, // The bundle identifier associated with your app
+      production: true // false // Specifies which environment to connect to: Production (if true) or Sandbox
+    }
+  }
   // liveQuery: {
   //   classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   // }
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
