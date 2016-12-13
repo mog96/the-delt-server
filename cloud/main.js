8ae1e49d7ab8f40000e237e76724e20c9dae0e86
Mateo Garcia <mateog@stanford.edu>
2016-12-12 16:21:56 -0800

    updated cloud code to send push after save to message class

--- cloud/main.js
+++ cloud/main.js
@@ -1,16 +1,39 @@
 
+/*
 Parse.Cloud.define('hello', function(req, res) {
     res.success('Hi');
 });
+*/
 
-Parse.Cloud.define('approveUser', function(request, response) {  
-    var creatingUser = request.user;
+Parse.Cloud.define('approveUser', function(request, response) {
+  var creatingUser = request.user;
 
-    var name = request.params.name;
-    var email = request.params.email; // string required
-    var username = request.params.username; // string required
-    var tempPass = request.params.tempPass; // string
+  var name = request.params.name;
+  var email = request.params.email; // string required
+  var username = request.params.username; // string required
+  var tempPass = request.params.tempPass; // string
 
-    var approvejs = require('../cloud/approve.js');
-    approvejs.approveUser(creatingUser, name, email, username, tempPass, response);
+  var approvejs = require('../cloud/approve.js');
+  approvejs.approveUser(creatingUser, name, email, username, tempPass, response);
+});
+
+Parse.cloud.afterSave("message", function(request) {
+  var pushQuery = new Parse.Query(Parse.Installation);
+  Parse.push.send({
+    where: pushQuery,
+    data: {
+      alert: {
+        "title": "Delts are chatting:",
+        "body": "Check it out."          /// LAST USER AND LAST MESSAGE
+      },
+      sound: "defualt"
+    }
+  }, {
+    success: function () {
+      // SUCCESSFUL PUSH
+    },
+    error: function (error) {
+      throw "CHAT PUSH ERROR: " + error.code + " : " + error.message;
+    }
+  });
 });
