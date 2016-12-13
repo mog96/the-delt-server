8e6e23b6de8001b53524fe0788e54bd9e7a74c7d
Mateo Garcia <mateog@stanford.edu>
2016-12-12 19:19:04 -0800

    push for chat working as expected. pushes on each new message created, silent if within x minutes after last push.

--- cloud/main.js
+++ cloud/main.js
@@ -17,23 +17,59 @@ Parse.Cloud.define('approveUser', function(request, response) {
   approvejs.approveUser(creatingUser, name, email, username, tempPass, response);
 });
 
-Parse.cloud.afterSave("message", function(request) {
+var 
+var sendNextPushAt = new Date();
+
+Parse.Cloud.afterSave('message', function(request) {
+  var sentAt = new Date(request.object.get('createdAt'));
+  console.log("SENT AT", sentAt);
+  console.log("NEXT PUSH AT", sendNextPushAt);
+
   var pushQuery = new Parse.Query(Parse.Installation);
-  Parse.push.send({
-    where: pushQuery,
-    data: {
-      alert: {
-        "title": "Delts are chatting:",
-        "body": "Check it out."          /// LAST USER AND LAST MESSAGE
+  var author = request.object.get('authorUsername');
+  var content = request.object.get('content');
+  console.log("AUTHOR", author);
+  console.log("MESSAGE", content);
+
+  if (sentAt > sendNextPushAt) {
+    sendNextPushAt = sentAt;
+    sendNextPushAt.setMinutes(sentAt.getMinutes() + 2);
+
+    Parse.Push.send({
+      where: pushQuery,
+      data: {
+        badge: 'Increment',
+        alert: {
+          title: 'Delts are chatting...',
+          body: author + ': ' + content
+        },
+        sound: 'default'
+      }
+    }, {
+      useMasterKey: true,
+      success: function () {
+        console.log('SUCCESSFUL CHAT PUSH SENT AT', new Date());
       },
-      sound: "defualt"
-    }
-  }, {
-    success: function () {
-      // SUCCESSFUL PUSH
-    },
-    error: function (error) {
-      throw "CHAT PUSH ERROR: " + error.code + " : " + error.message;
-    }
-  });
+      error: function (error) {
+        throw 'CHAT PUSH ERROR: ' + error.code + ' : ' + error.message;
+      }
+    });
+
+  } else { // Send silent push to increment badge number
+    Parse.Push.send({
+      where: pushQuery,
+      data: {
+        badge: 'Increment',
+        'content-available': 0
+      }
+    }, {
+      useMasterKey: true,
+      success: function () {
+        console.log('SUCCESSFUL CHAT SILENT PUSH SENT AT', new Date());
+      },
+      error: function (error) {
+        throw 'CHAT SILENT PUSH ERROR: ' + error.code + ' : ' + error.message;
+      }
+    });
+  }
 });
